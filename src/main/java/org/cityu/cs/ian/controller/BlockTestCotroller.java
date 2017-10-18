170e10f6448cb47d1ceb14bdf3ea5d1e3e1cfa88
epmouse <18622117113@126.com>
2017-10-18 23:06:05 +0800

    待完成功能梳理

--- src/main/java/org/cityu/cs/ian/controller/BlockTestCotroller.java
+++ src/main/java/org/cityu/cs/ian/controller/BlockTestCotroller.java
@@ -6,8 +6,10 @@ import org.cityu.cs.ian.service.Threads.IBlockAcceptService;
 import org.cityu.cs.ian.service.Threads.ITransactionService;
 import org.cityu.cs.ian.util.Constant;
 import org.cityu.cs.ian.util.PropertiesUtil;
+import org.cityu.cs.ian.util.ResponseMsgUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,7 +28,7 @@ public class BlockTestCotroller {
     private IBlockAcceptService blockAcceptService;
 
 
-    @RequestMapping(value = "transaction", method = RequestMethod.POST)
+    @RequestMapping(value = "add/transaction", method = RequestMethod.POST)
     public String transactionAccept(@RequestBody Transaction1 transaction1) {
         if (transactionService.verifySign(transaction1)) {
             transactionService.saveTransactionToList(transaction1);
@@ -35,30 +37,55 @@ public class BlockTestCotroller {
         return Constant.ERR_RESPONSE;
     }
 
-    @RequestMapping(value = "block", method = RequestMethod.POST)
+    @RequestMapping(value = "add/block", method = RequestMethod.POST)
     public Map<String, String> acceptBlock(@RequestBody BlockBean block) {
         if (blockAcceptService.verifyBlock(block)) {
             blockAcceptService.interruptPow();
             boolean b = blockAcceptService.saveBlock(block);
             if (b) {
-                return getResponseMap(true, null);
+                return ResponseMsgUtils.getResponseMap(true, null);
             } else {
                 System.out.println("BlockAcceptServicee中saveBlock（）方法文件写入失败，请查看异常");
 
-                return getResponseMap(false,"文件写入失败");
+                return ResponseMsgUtils.getResponseMap(false,"文件写入失败");
             }
         } else {
-            return getResponseMap(false,"block验证失败");
+            return ResponseMsgUtils.getResponseMap(false,"block验证失败");
         }
     }
 
-    private Map<String, String> getResponseMap(boolean b, String errDetails) {
-        Map<String, String> backMap = new HashMap<>();
-        backMap.put("status", b ? "ok" : "err");
-        backMap.put("details", errDetails == null ? "" : errDetails);
-        backMap.put("url", PropertiesUtil.readValue("config.properties", "currentServerUrl"));
-        return backMap;
+
+    @RequestMapping(value = "syncBlock/{blockName}", method = RequestMethod.GET)
+    public void syncBlocks(@PathVariable("blockName") String blockName) {
+        //todo-文件的下载操作，找到对应名字的区块文件并以流的形式返回
     }
+    @RequestMapping(value = "syncBlock/Highest", method = RequestMethod.GET)
+    public String getTopBlockHeight(String blockName) {
+        //todo-返回目前最高区块的高度，方便其他server根据目前的缺失情况，循环调用下载接口，下载区块
+
+        return "";
+    }
+    @RequestMapping(value = "syncBlock/blockDetail", method = RequestMethod.GET)
+    public String getBlockByHeight(String blockName) {
+        //todo-根据blockheight 查询出对应block  返回json
+
+        return "";
+    }
+    @RequestMapping(value = "syncBlock/getTransaction", method = RequestMethod.GET)
+    public String getTransaction(String transactionid) {
+        //todo-根据transactionid查询这条transactiong的信息 返回 json
+
+        return "";
+    }
+    @RequestMapping(value = "syncBlock/all", method = RequestMethod.GET)
+    public String getAllBlockForClient(String page) {  //1为最新的10条,依次往下
+        //todo-给客户端提供所有block的json串。（考虑分页）从后往前，先发最新的
+        return "";
+    }
+
+//todo-除以上contrller操作外 还需要做调用下载和获取最高区块的两个接口的操作
+
+
 
 //    1、同步block  下载没有的blcok文件。（1提供下载接口， 2下载并保存）
 //    2、给客户端提供所有block的json串。（考虑分页）从后往前，先发最新的
